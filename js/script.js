525907b45310a7ae053e6fb3ff5639ceabebd9eb
Firas Melliti <mellitifiras@protonmail.com>
2021-09-02 18:38:25 -0400

    using the geolocation api and adding the map using leaflet libary

--- js/script.js
+++ js/script.js
@@ -9,3 +9,23 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(
+        // func happens when geo location is accepted
+        function(position){
+            const {latitude, longitude} = position.coords;
+            const coords = [latitude, longitude];
+            const map = L.map('map').setView(coords, 13);
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',{
+                attribution: '<a style="color:red;text-decoration:none" href="https://www.github.com/firasmlt" target="_blank">My GitHub</a>'
+            }).addTo(map);
+            
+        },
+        // func happens when geo location is declined
+        function(){
+            alert(`We couldn't get your location`)
+        }
+            
+    )
+}
