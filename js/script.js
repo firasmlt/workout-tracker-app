4cb284233692829b8aca3c2d5855e12e5ac6f7c4
Firas Melliti <mellitifiras@protonmail.com>
2021-09-03 02:03:35 -0400

    refactoring the code - implementing oop

--- js/script.js
+++ js/script.js
@@ -10,49 +10,57 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+// let map, mapEvent;
 
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(
-        // func happens when geo location is accepted
-        function(position){
-            const {latitude, longitude} = position.coords;
-            const coords = [latitude, longitude];
-            map = L.map('map').setView(coords, 13);
-            L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',{
-                attribution: '<a style="color:red;text-decoration:none" href="https://www.github.com/firasmlt" target="_blank">My GitHub</a>'
-            }).addTo(map);
-            map.on('click', function(mapE){
-                mapEvent = mapE;
-                form.classList.remove('hidden');
-                inputDistance.focus();
-            })
-        },
-        // func happens when geo location is declined
-        function(){
-            alert(`We couldn't get your location`)
+class App{
+    #map;
+    #mapEvent;
+    constructor(){
+        this._getPosition();
+        form.addEventListener('submit', this._newWorkout.bind(this))
+        inputType.addEventListener('change', this._toggleElevationField)
+    }
+
+    _getPosition(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), ()=>alert(`We couldn't get your location`));
         }
-            
-    )
+    }
+
+    _loadMap(position){
+        const {latitude, longitude} = position.coords;
+        const coords = [latitude, longitude];
+        this.#map = L.map('map').setView(coords, 13);
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',{
+            attribution: '<a style="color:red;text-decoration:none" href="https://www.github.com/firasmlt" target="_blank">My GitHub</a>'
+            }).addTo(this.#map);
+        this.#map.on('click', this._showForm.bind(this))
+    }
+
+    _showForm(mapE){
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+    _toggleElevationField(){
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+    }
+    _newWorkout(e){
+        e.preventDefault();
+        
+        inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value = '';
+        const {lat, lng} = this.#mapEvent.latlng;
+            L.marker([lat, lng]).addTo(this.#map).bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'cycling-popup'
+        })).setPopupContent('workout').openPopup();}
 }
 
-
-form.addEventListener('submit', function(e){
-    e.preventDefault();
+const app = new App();
 
 
-    inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value = '';
-    const {lat, lng} = mapEvent.latlng;
-        L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'cycling-popup'
-    })).setPopupContent('workout').openPopup();})
 
-
-inputType.addEventListener('change', function(){
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
-})
