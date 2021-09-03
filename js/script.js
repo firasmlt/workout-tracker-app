4f624a9163e4112ac7687d051aa8dc46aca78e30
Firas Melliti <mellitifiras@protonmail.com>
2021-09-02 21:03:14 -0400

    Rendering Workout Input Form Functionality

--- js/script.js
+++ js/script.js
@@ -10,25 +10,22 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(
         // func happens when geo location is accepted
         function(position){
             const {latitude, longitude} = position.coords;
             const coords = [latitude, longitude];
-            const map = L.map('map').setView(coords, 13);
+            map = L.map('map').setView(coords, 13);
             L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',{
                 attribution: '<a style="color:red;text-decoration:none" href="https://www.github.com/firasmlt" target="_blank">My GitHub</a>'
             }).addTo(map);
-            map.on('click', function(mapEvent){
-                const {lat, lng} = mapEvent.latlng;
-                L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
-                    maxWidth: 250,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: 'cycling-popup'
-                })).setPopupContent('workout').openPopup();
+            map.on('click', function(mapE){
+                mapEvent = mapE;
+                form.classList.remove('hidden');
+                inputDistance.focus();
             })
         },
         // func happens when geo location is declined
@@ -38,3 +35,24 @@ if(navigator.geolocation){
             
     )
 }
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+
+    inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value = '';
+    const {lat, lng} = mapEvent.latlng;
+        L.marker([lat, lng]).addTo(map).bindPopup(L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'cycling-popup'
+    })).setPopupContent('workout').openPopup();})
+
+
+inputType.addEventListener('change', function(){
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+})
